510483fa1018eb4229b335c92543f61dab977b27
Lenovo-User <Lenovo-User@DESKTOP-QJ3UI1M>
2024-01-24 17:39:40 +0200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.CONSTRUCTOR
+{
+    internal class Book
+    {
+        public string Title;
+        public string Author;
+        public int Pages;
+
+
+
+        public Book(string aTitle, string aAuthor , int aPages) 
+        {
+            Title = aTitle;
+            Author = aAuthor;
+            Pages = aPages;
+        }
+
+
+    
+
+       
+  
+
+       
+
+    
+
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.CONSTRUCTOR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("harry potter", "jk rowlling", 400);
+
+            Console.WriteLine(book1.Title);
+            Console.Read();
+       
+            
+            
+            
+
+
+        }
+    }
+}
